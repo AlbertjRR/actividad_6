64b4241488eae3cb2f1574c8bbef40ad897dba21
AlbertjRR <albert_jr001@hotmail.com>
2024-07-04 15:52:29 -0400

    Update AlbertRodriguez_4.cpp

--- AlbertRodriguez_4.cpp
+++ AlbertRodriguez_4.cpp
@@ -1,16 +1,14 @@
-//Ejercicio 4
-
-
+//nuemro 4
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
 int main() {
-    char cadena[100];
+    char cadena[50];
 
-    printf("David por favor ingresa una cadena de texto: ");
-    fgets(cadena, 100, stdin);
+    printf("ingresar cadena: ");
+    fgets(cadena, 50, stdin);
 
     int longitud = strlen(cadena);
     int inicio = 0;
@@ -20,7 +18,7 @@ int main() {
     }
 
     memmove(cadena, cadena + inicio, longitud - inicio + 1);
-    printf("David la cadena sin espacios en blanco al principio es: %s\n", cadena);
+    printf("el resultado de la cadena es: %s\n", cadena);
 
     return 0;
 }
