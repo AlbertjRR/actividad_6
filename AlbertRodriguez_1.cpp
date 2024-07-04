1f630167cd541fccf5797763e14c8a994fd28586
AlbertjRR <albert_jr001@hotmail.com>
2024-07-04 15:34:47 -0400

    Update AlbertRodriguez_1.cpp

--- AlbertRodriguez_1.cpp
+++ AlbertRodriguez_1.cpp
@@ -1,24 +1,23 @@
-//Ejercicio 1
-
+// numero1
 
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    char cadena[100];
+    char cadena[50];
     int n;
 
-    printf("Hola David por favor ingresa una cadena de texto: ");
-    fgets(cadena, 100, stdin);
+    printf("ingresar cadena: ");
+    fgets(cadena, 50, stdin);
 
-    printf("David por favor ingresa la cantidad de caracteres a extraer: ");
+    printf("ingresar cantidad de caracteres a extraer: ");
     scanf("%d", &n);
 
     if (n > strlen(cadena)) {
-        printf("Error: Lo siento David la cantidad de caracteres a extraer supera la longitud de la cadena.\n");
+        printf("Error: los caracteres a extraer es superior a la longitud de la cadena.\n");
     } else {
         cadena[n] = '\0'; 
-        printf("David los primeros %d caracteres son: %s\n", n, cadena);
+        printf("los primeros %d caracteres son: %s\n", n, cadena);
     }
     return 0;
 }
